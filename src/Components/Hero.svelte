42720e7695ca93e9373e95790d002ffb6aeb5774
TylrPopcorn <tyler.mcmillan99@yahoo.com>
2025-04-03 14:08:35 -0400

    Hero section

---
+++ src/Components/Hero.svelte
@@ -0,0 +1,22 @@
+<!-- HERO SECTION -->
+
+<!-- HTML -->
+<div class="HeroSection">
+     <div class="container Hero-Center-Content">
+        <h1 class="hero-title">
+            Market Street <br> Barbers
+        </h1>
+        <a
+            href="https://booksy.com/en-us/1291904_market-street-barbers_barber-shop_26503_concord"
+            class="hero-book-btn btn btn-custom"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            BOOK A CUT
+        </a>
+    </div>
+    <div class="container Hero-Center-Content-2">
+        <div class="hero-arrow" alt=""></div>
+    </div>
+</div>
+
