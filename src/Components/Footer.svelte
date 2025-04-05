79b22123f26e352097578e040562a7d888caffdb
TylrPopcorn <tyler.mcmillan99@yahoo.com>
2025-04-05 00:57:12 -0400

    footer

---
+++ src/Components/Footer.svelte
@@ -0,0 +1,57 @@
+<!-- FOOTER SECTION -->
+<!-- HTML -->
+<div id="footer" class="Footer">
+    <div class="container Footer-wrapper">
+        <div class="container row Footer-row">
+          <div class="col-12 col-sm-6 col-md-2 Footer-logo-wrapper">
+            <div class="Footer-logo" />
+          </div>
+          <div class="col-md-4 Footer-Hours">
+            <h5 class="Footer-Header">HOURS</h5>
+            <p class="Footer-Content">Tuesday - 8:00 am to 7:00 pm</p>
+            <p class="Footer-Content">Wednesday - 8:00 am to 5:00 pm</p>
+            <p class="Footer-Content">Thursday - 8:00 am to 7:00 pm</p>
+            <p class="Footer-Content">Friday - 8:00 am to 6:00 pm</p>
+            <p class="Footer-Content">Saturday - 7:00 am to 3:00 pm</p>
+          </div>
+          <div class="col-md-3 Footer-Contact-Social">
+            <div class="Footer-Contact">
+              <h5 class="Footer-Header">CONTACT</h5>
+              <p class="Footer-Content">704-260-7464</p>
+              <p class="Footer-Content">Marketstreetbarbers24@gmail.com</p>
+              <a
+                class="Footer-Content"
+                href="https://www.google.com/maps/place/15+Market+St+SW,+Concord,+NC+28025/data=!4m2!3m1!1s0x88540e5147113705:0x8566b0d1bfccd174?entry=s&sa=X&ved=1t:78214&ictx=111"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                5 Market St SW Concord, NC 28025 <br />
+                United States
+              </a>
+            </div>
+            <div class="Footer-Social">
+              <h5 class="Footer-Header">SOCIAL</h5>
+              <a
+                class="Footer-Social-Instagram"
+                href="https://www.instagram.com/marketstreetbarbers/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span></span>
+              </a>
+            </div>
+          </div>
+          <div class="col-md-2 Footer-Maps">
+            <div class="map-container">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3264.9794312986884!2d-80.57951438477892!3d35.40875128024588!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88541d089f80e281%3A0x48a1f1c18b1636b8!2s5%20Market%20St%20SW%2C%20Concord%2C%20NC%2028025!5e0!3m2!1sen!2sus!4v1613942292645!5m2!1sen!2sus"
+                allowFullScreen=""
+                loading="lazy"
+                class="MAP"
+                title="Google Maps location of Market Street Barbers in Concord, NC"
+              ></iframe>
+            </div>
+          </div>
+        </div>
+    </div>
+</div>
