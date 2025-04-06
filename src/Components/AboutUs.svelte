95729e34d8573c161a4c8a22e1b5e3000fec21f9
TylrPopcorn <tyler.mcmillan99@yahoo.com>
2025-04-06 11:20:11 -0400

    about us

---
+++ src/Components/AboutUs.svelte
@@ -0,0 +1,14 @@
+<!-- ABOUT US SECTION -->
+<!-- HTML -->
+<div id="about" class="AboutUs">
+    <div class="ABT-title-section">
+        <div class="container ABT-Center-Content">OUR STORY</div>
+    </div>
+    <div class="container ABT-pg-section">
+        At Market Street Barbers, we provide quality haircuts and shaves in a
+        welcoming space where everyone feels like family. Our mission is to
+        offer top-notch service while fostering community connections, ensuring
+        every visit feels like home.
+    </div>
+	<div class="ABT-img"></div>
+</div>
