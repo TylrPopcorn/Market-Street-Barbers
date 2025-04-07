4fd0dd17dd057cc3b7e3418f8c1dcc8a8e55fead
TylrPopcorn <tyler.mcmillan99@yahoo.com>
2025-04-07 10:47:38 -0400

    our team

---
+++ src/Components/OurTeam.svelte
@@ -0,0 +1,43 @@
+<!-- OUR TEAM SECTION -->
+<!-- HTML -->
+ <div class="OurTeam">
+    <div class="container OurTeam-Header">MEET THE TEAM</div>
+      <div class="container Cards row g-4">
+        <div class="col-md-4 Col">
+          <div class="Card">
+            <div class="card-1-img"></div>
+            <div class="Stylist-Text">Stylist</div>
+          </div>
+          <div class="Card-Info">
+            <p class="Card-Title">EASTON NESBITT</p>
+            <p class="Card-Paragraph">Co-Founder</p>
+            <a
+              href="https://booksy.com/en-us/instant-experiences/widget/538459?instant_experiences_enabled=true&ig_ix=true"
+              class="OurTeam-book-btn btn btn-custom"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              BOOK A CUT
+            </a>
+          </div>
+        </div>
+        <div class="col-md-4 Col" style="margin-top: 15rem;">
+          <div class="Card">
+            <div class="card-2-img"></div>
+            <div class="Stylist-Text">Stylist</div>
+          </div>
+          <div class="Card-Info">
+            <p class="Card-Title">KEVIN VILLACIS</p>
+            <p class="Card-Paragraph">Co-Founder</p>
+            <a
+              href="https://booksy.com/en-us/instant-experiences/widget/996910"
+              class="OurTeam-book-btn btn btn-custom"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              BOOK A CUT
+            </a>
+          </div>
+        </div>
+    </div>
+</div>
