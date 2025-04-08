053144234e58c8e320287e21dda4e50a6b0a7142
TylrPopcorn <tyler.mcmillan99@yahoo.com>
2025-04-08 09:11:29 -0400

    Gallery

---
+++ src/Components/Gallery.svelte
@@ -0,0 +1,53 @@
+<!-- GALLERY SECTION -->
+<div class="Gallery">
+    <div
+    	id="barberCarousel"
+        class="carousel slide"
+        data-bs-ride="carousel"
+    >
+        <ol class="carousel-indicators">
+          <li
+            data-bs-target="#barberCarousel"
+            data-bs-slide-to="0"
+            class="active"
+          ></li>
+          <li data-bs-target="#barberCarousel" data-bs-slide-to="1"></li>
+          <li data-bs-target="#barberCarousel" data-bs-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <div class="d-block w-100 carousel_IMG1" src="" alt=""></div>
+          </div>
+          <div class="carousel-item">
+            <div class="d-block w-100 carousel_IMG2" src="" alt=""></div>
+          </div>
+          <div class="carousel-item">
+            <div class="d-block w-100 carousel_IMG3" src="" alt=""></div>
+          </div>
+        </div>
+        <a
+          class=" carousel-control-prev"
+          href="#barberCarousel"
+          role="button"
+          data-bs-slide="prev"
+        >
+          <span
+            class="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span class="sr-only"></span>
+        </a>
+        <a
+          class="carousel-control-next"
+          href="#barberCarousel"
+          role="button"
+          data-bs-slide="next"
+        >
+          <span
+            class="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span class="sr-only"></span>
+        </a>
+    </div>
+</div>
